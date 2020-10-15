5fd5eeecce8c9162532c2e819f6227d205e80af5
Carl J <carlj1881@yahoo.com>
2020-10-15 22:06:39 +0000

    created var for albums in API

--- lib/api.rb
+++ lib/api.rb
@@ -3,12 +3,12 @@
 class API
   
   def self.fetch_albums
-    
     url = "https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=david_bowie"
     binding.pry
     uri = URI(url)
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
+    array_of_albums = hash["album"]
   end
   
   
