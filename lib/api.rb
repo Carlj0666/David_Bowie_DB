dc6cafb7a213cd399a93784c50fa258e7c5786e5
Carl J <carlj1881@yahoo.com>
2020-10-15 21:43:32 +0000

    defined start in CLI, defined self.fetch_albums in API, created url var, uri var, response and hash var.

--- lib/api.rb
+++ lib/api.rb
@@ -2,5 +2,14 @@
 
 class API
   
+  def self.fetch_albums
+    
+    url = "https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=david_bowie"
+    binding.pry
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    hash = JSON.parse(response)
+  end
+  
   
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,7 @@ class CLI
   
   def start
     puts "The David Bowie DB is happening now, not tomorrow"
-    
+    API.fetch_albums
   end
   
   
