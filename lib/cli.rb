5b0cd01b133eea6ef8f0c8d2629297c9bd2ea104
Carl J <carlj1881@yahoo.com>
2020-10-16 21:38:09 +0000

    continue logic build for CLI class methods, (show_list_of_albums)

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,8 +7,6 @@ class CLI
     self.db_menu
   end
   
-  db_lyrics_mash_array = []
-  
   def db_menu
     #give user option to see list of records or a list of the records by year
     puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you"
@@ -16,19 +14,33 @@ class CLI
     sleep(1)
     puts "Type 'records' for a list of my albums"
     sleep(1)
-    puts "Or type 'records_by_date' for a date sorted list of my albums"
+    # puts "Or type 'records_by_date' for a date sorted list of my albums"
     
-    user_entry = gets.strip.downcase
+    user_entry = gets.strip.downcase #gets the user entry
     
-    if user_entry == "records"
+    if user_entry == "records" #logic for user input choice, chose records or sorted
       puts "This chaos is killing me"
       #display unsorted list
       
-      elsif user_entry == "records_by_date"
+      show_list_of_albums
       
-      puts "Your all hung up on her mind filters"
+      # elsif user_entry == "records_by_date"
+      # puts "Your all hung up on her mind filters"
+      #display sorted list
+    end
+  end
+    
+    def show_list_of_albums
+      
+      #access the list
+      DB_Record.all.each do |record|
+        #binding.pry
+        puts record.name
+      #print each album out
+      end
     end
       
-  end
+      #show sorted list of albums
+      #use .sort
   
 end
