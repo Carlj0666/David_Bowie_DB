9e4e529ddcec0f0f870d7a7177b8955f9ffce5a7
Carl J <carlj1881@yahoo.com>
2020-10-17 23:04:30 +0000

    Continuuing work in CLI, working logic for decision tree for user, while debugging, decided to create new method to accommidate for displaying the full record object

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,17 +8,15 @@ class CLI
   end
   
   def db_menu
-    #give user option to see list of records or a list of the records by year
     puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you"
     
     sleep(1)
     puts "Type 'records' for a list of my albums"
-    # sleep(1)
-    # puts "Or type 'records_by_year' for a date sorted list of my albums"
     
     user_entry = gets.strip.downcase #gets the user entry
     
     if user_entry != "records" #logic for user input choice, chose records or sorted
+    puts "I ask for so little, back to the start!"
       db_menu
     else
       puts "This chaos is killing me"
@@ -28,64 +26,47 @@ class CLI
       
       user_record_selector
       
-      # elsif user_entry == "records_by_year"
-      # puts "Your all hung up on her mind filters"
-      # record_date_sorter
+
     end
   end
     
-    def show_list_of_albums
-      
-      #access the list
-      DB_Record.all.each.with_index(1) do |record, index|
-        #binding.pry
-        puts "#{index} - #{record.name}"
-      #print each album out
-      end
+  def show_list_of_albums
+    DB_Record.all.each.with_index(1) do |record, index| #iterate through indexes of the DB_Record array
+    puts "#{index} - #{record.name}"#print each album out
     end
+  end
       
-    def user_record_selector
-      puts "Select a number to access the corresponding album"
-      user_index_selection = gets.strip.downcase.to_i - 1
       
-      if !user_index_selection.between?(0, 24)
-        user_record_selector
-        else
+  def user_record_selector
+    puts "Select a number between 1 - 24 to access the corresponding album"
+    user_index_selection = gets.strip.downcase.to_i - 1
       
-        current_record_instance = DB_Record.all[user_index_selection]
-      
-        puts "You've chosen: #{current_record_instance}"
-        sleep(2)
-        
-        puts "Type records to go back to the list, or type exit to end the program"
-        user_selection = gets.strip.downcase
-       
-        if user_selection == "records"
-          show_list_of_albums
-          
-          else puts "Type records to go back to the list, or type exit to end the program"
-          user_selection = gets.strip.downcase
-          
-        # if user_index_selection == "exit"
-        
-        # more_record_info = DB_Record.all[current_record_instance]
-        #binding.pry
-      end
-      end
-    end
-      
-    # def record_date_sorter
-    #   date_array = []
-    #   DB_Record.all.each do |record, record_name|
-    #     date_to_integer = record.release_year.to_i
-    #     record_name = record.name
-    #     date_array << {record_name => date_to_integer
-    #     binding.pry
-    #   end
-    #   sorted_dates = date_array.sort
-    #   sorted_dates
-    # end
-      
+    if !user_index_selection.between?(0, DB_Record.all.length - 1)
+      user_record_selector
     
+    else
+      binding.pry
+      current_record_instance = DB_Record.all[user_index_selection]
+      puts "You've chosen: #{current_record_instance.name}"
+      
+    end
+  end
+      
   
 end
+
+
+
+# current_record_instance
+#       sleep(1)
+        
+#       puts "Type 'records' to go back to the list"
+#       user_selection = gets.strip.downcase
+       
+#       if user_selection == "records"
+#         user_record_selector
+#         puts "Type records to go back to the list, or type any other key to end the program"
+#         user_selection = gets.strip.downcase
+#       else 
+#         return
+#       end
