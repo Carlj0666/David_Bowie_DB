7bbc5b768cce889470cd715fb63f1ba3a4cff7cb
Carl J <carlj1881@yahoo.com>
2020-10-18 00:11:39 +0000

    completed exit logic, was simple. Working on formatting text to user from CLI.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,24 +2,28 @@ class CLI
   
   def start #start instance method
     puts "The David Bowie DB is happening now, not tomorrow" #prints welcome messsage
-    sleep(1) #waits to run API for (seconds)
+    puts "\n"
+    sleep(3) #waits to run API for (seconds)
     API.get_albums #runs API.rb to grab info from API and makes DB_Record instances
     self.db_menu
   end
   
   def db_menu
-    puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you"
-    
+    puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you."
+    puts "\n"
     sleep(1)
-    puts "Type 'records' for a list of my albums"
+    puts "Type 'records' for a list of my albums."
     
     user_entry = gets.strip.downcase #gets the user entry
     
     if user_entry != "records" #logic for user input choice, chose records or sorted
-    puts "I ask for so little, back to the start!"
+    puts "I ask for so little. Let's start from th beginning"
+    puts "\n"
+    sleep(2)
       db_menu
     else
-      puts "This chaos is killing me"
+      puts "This chaos is killing me..."
+      puts "\n"
       
       show_list_of_albums
       
@@ -39,6 +43,7 @@ class CLI
       
   def user_record_selector
     puts "Select a number between 1 - 24 to access the corresponding album"
+    puts "\n"
     user_index_selection = gets.strip.downcase.to_i - 1
       
     if !user_index_selection.between?(0, DB_Record.all.length - 1)
@@ -47,25 +52,31 @@ class CLI
     else
       current_record_instance = DB_Record.all[user_index_selection]
       puts "You've chosen: #{current_record_instance.name}"
-      
+      puts "\n"
       record_detail_viewer(current_record_instance)
     end
   end
       
+      
   def record_detail_viewer(current_record)
-    puts "I've been waiting for you, for such a long time now"
-    puts "\n Record Name: #{current_record.name} \n Genre: #{current_record.genre} \n Release Year: #{current_record.release_year} \n Label: #{current_record.label} \n Description: #{current_label.description}"
+    puts "I've been waiting for you, for such a long time now \n"
+    puts "Artist: Yours Truly \n Record Name: #{current_record.name} \n Genre: #{current_record.genre} \n Release Year: #{current_record.release_year} \n Label: #{current_record.label} \n Description: #{current_record.description}\n"
     sleep(1)
-    puts "Type 'records' to go back to the list or any key to end"
+
+    puts "Type 'records' to go back to the list or 'exit' to end.\n"
     user_selection = gets.strip.downcase
     
-    if user_selection != "records"
-      puts "I dream of something like that"
-      record_detail_viewer(current_record)
+    if user_selection == "records"
+      puts "Where do we go to now?\n"
+      show_list_of_albums
+      user_record_selector
+      record_detail_viewer
+      
+    elsif user_selection == "exit"
+      exit!
     else
-      #show_list_of_albums
-      #user_record_selector
-      #record_detail_viewer
+      puts "I dream of something like that.\n"
+      record_detail_viewer(current_record)
     end
   end
   
@@ -73,18 +84,3 @@ class CLI
   
 end
 
-
-
-# current_record_instance
-#       sleep(1)
-        
-#       puts "Type 'records' to go back to the list"
-#       user_selection = gets.strip.downcase
-       
-#       if user_selection == "records"
-#         user_record_selector
-#         puts "Type records to go back to the list, or type any other key to end the program"
-#         user_selection = gets.strip.downcase
-#       else 
-#         return
-#       end
