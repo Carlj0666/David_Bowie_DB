51bd3d975e0852abe8f025bdc4ddd8ffe87f0d6b
Carl J <carlj1881@yahoo.com>
2020-10-19 21:35:47 +0000

    Refactored code

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,21 +5,26 @@ class CLI
     puts "\n"
     sleep(3) #waits to run API for (seconds)
     API.get_albums #runs API.rb to grab info from API and makes DB_Record instances
+    puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you."
+    sleep(1)
     self.db_menu
   end
   
   def db_menu
-    puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you."
-    sleep(1)
-    puts "Type 'records' for a list of my albums."
+    
+    puts "Type 'records' for a list of my albums, or type 'exit' to quit."
     
     user_entry = gets.strip.downcase #gets the user entry
     
-    if user_entry != "records" #logic for user input choice, chose records or sorted
-    puts "I ask for so little. Let's start from the beginning."
-    puts "\n"
-    sleep(2)
+    if user_entry == "exit"
+      puts "goodbye"
+      
+    elsif user_entry != "records" #logic for user input choice, chose records or sorted
+      puts "I ask for so little. Let's start from the beginning."
+      puts "\n"
+      sleep(2)
       db_menu
+      
     else
       puts "This chaos is killing me..."
       puts "\n"
@@ -28,7 +33,7 @@ class CLI
       
       user_record_selector
       
-      record_detail_viewer
+      db_menu
       
     end
   end
@@ -45,8 +50,9 @@ class CLI
     #binding.pry
 
     user_index_selection = gets.strip.downcase.to_i - 1 
-
-    if !user_index_selection.between?(0, DB_Record.all.length - 1) && !user_index_selection.include?(/\D/) #WORKING THIS LINE
+ #binding.pry
+     if !user_index_selection.between?(0, DB_Record.all.length - 1)
+        # && !user_index_selection.include?(/\D/) #WORKING THIS LINE
       user_record_selector
     
     else
@@ -63,24 +69,8 @@ class CLI
     puts "Artist: Yours Truly db \n Record Name: #{current_record.name} \n Genre: #{current_record.genre} \n Release Year: #{current_record.release_year} \n Label: #{current_record.label} \n Description: #{current_record.description}\n"
     sleep(1)
 
-    puts "Type 'records' to go back to the list or 'exit' to end.\n"
-    user_selection = gets.strip.downcase
-    
-    if user_selection == "records"
-      puts "Where do we go to now?\n"
-      show_list_of_albums
-      user_record_selector
-      record_detail_viewer
-      
-    elsif user_selection == "exit"
-      puts '"If it works, its out of date."' "― David Bowie" 
-      exit!
-    else
-      puts "I dream of something like that.\n"
-      record_detail_viewer(current_record)
-    end
   end
   
-  #need to figure out how to ignore letters or not numbers in album 
+  #Look up colorize, check 
 end
 
