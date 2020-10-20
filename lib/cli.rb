74193ecb2ccf1b1c8eec6e429dcb5de8d91e05bc
Carl J <carlj1881@yahoo.com>
2020-10-20 21:00:23 +0000

    Making formating changes to messages

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,6 +6,7 @@ class CLI
     sleep(3) #waits to run API for (seconds)
     API.get_albums #runs API.rb to grab info from API and makes DB_Record instances
     puts "Hello, I'm the machine spirit of David Bowie come to share my albums with you."
+    puts "\n"
     sleep(1)
     self.db_menu
   end
@@ -17,19 +18,25 @@ class CLI
     
     user_entry = gets.strip.downcase #gets the user entry
     
-    if user_entry == "exit"
+    if user_entry == "exit" #option to exit in main menu
+      puts "\n"
       puts '"If it works it\'s out of date"' "\n- David Bowie" 
       
     elsif user_entry != "records" #logic for user input choice, chose records or sorted
+      
       puts "I ask for so little. Let's start from the beginning."
       puts "\n"
       sleep(2)
       db_menu
       
     else
-      puts "This chaos is killing me..."
       puts "\n"
-      
+      puts '"This chaos is killing me..."'
+      sleep(3)
+      puts "\n"
+      puts "Select a number between 1 - 24 to access the corresponding album."
+      sleep(5)
+      puts "\n"
       show_list_of_albums
       
       user_record_selector
@@ -47,32 +54,32 @@ class CLI
       
       
   def user_record_selector
-    puts "Select a number between 1 - 24 to access the corresponding album.\n"
-    #binding.pry
-
+    
+    puts "Select a number between 1 - 24 to access the corresponding album."
+    puts "\n"
+    
     user_index_selection = gets.strip.downcase.to_i - 1 
- #binding.pry
-     if !user_index_selection.between?(0, DB_Record.all.length - 1)
-        # && !user_index_selection.include?(/\D/) #WORKING THIS LINE
+
+    puts '"I\'ve been waiting for you, for such a long time now."'
+    puts "\n"
+    
+    if !user_index_selection.between?(0, DB_Record.all.length - 1)
       user_record_selector
     
     else
       current_record_instance = DB_Record.all[user_index_selection]
       puts "You've chosen: #{current_record_instance.name}\n"
       record_detail_viewer(current_record_instance)
-    # end
     end
   end
       
       
   def record_detail_viewer(current_record)
-    puts "I've been waiting for you, for such a long time now.\n"
-    puts "Artist: Yours Truly db \n Record Name: #{current_record.name} \n Genre: #{current_record.genre} \n Release Year: #{current_record.release_year} \n Label: #{current_record.label} \n Description: #{current_record.description}\n"
+    puts "Artist: db \n Record Name: #{current_record.name} \n Genre: #{current_record.genre} \n Release Year: #{current_record.release_year} \n Label: #{current_record.label} \n Description: #{current_record.description}\n"
     puts "\n"
     sleep(1)
 
   end
   
-  #Look up colorize, check 
 end
 
