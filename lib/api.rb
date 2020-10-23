b3a9088908b538060f5f4f8dbd60b2ccfe941074
Carl J <carlj1881@yahoo.com>
2020-10-23 23:34:24 +0000

    changed API, working code to reflect changes

--- lib/api.rb
+++ lib/api.rb
@@ -1,22 +1,27 @@
 class API
   
   def self.get_albums
-    url = "https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=david_bowie"
+    #url = "https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=david_bowie"
+    url = "https://itunes.apple.com/lookup?id=551695&entity=album"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
     
-    albums_array = hash["album"]
+    albums_array = hash["results"]
     
     albums_array.each do |album|
       
       record = DB_Record.new
-      record.name = album["strAlbum"]
-      record.release_year = album["intYearReleased"]
-      record.description = album["strDescriptionEN"]
-      record.genre = album["strGenre"]
-      record.label = album["strLabel"]
+      
+      record.name = album["collectionName"]
+      record.release_year = album["releaseDate"]
+      record.description = album["collectionViewUrl"]
+      record.genre = album["primaryGenreName"]
+      record.label = album["copyright"]
+      
+      
     end
+    
   end
   
   
